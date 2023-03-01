e1eaa74d495b03731ba9f865d426bb229b8f4105
MackInTech <jerrykmack803@yahoo.com>
2023-02-28 21:08:56 -0500

    created 1st BasketballPlayer

--- src/athleticScholarshipPackage/BasketballPlayer.java
+++ src/athleticScholarshipPackage/BasketballPlayer.java
@@ -1,4 +1,69 @@
 package athleticScholarshipPackage;
 
 public class BasketballPlayer extends Athlete {
+    private int avgPointsPerGame;
+    private int avgAssistsPerGame;
+    private int avgOffensiveReboundsPerGame;
+    private int avgDefensiveReboundsPerGame;
+    private int avgStealsPerGame;
+    private int avgBlocksPerGame;
+
+    public BasketballPlayer() {
+    }
+
+    public BasketballPlayer(int rank, String sport, String firstName, String lastName,
+                            String hometown, String phoneNumber, String playerPosition,
+                            double height, int weight, String highSchool, boolean signed,
+                            String college, int avgPointsPerGame, int avgAssistsPerGame,
+                            int avgOffensiveReboundsPerGame, int avgDefensiveReboundsPerGame,
+                            int avgStealsPerGame, int avgBlocksPerGame) {
+        super(rank, sport, firstName, lastName, hometown, phoneNumber, playerPosition,
+                height, weight, highSchool, signed, college);
+        this.avgPointsPerGame = avgPointsPerGame;
+        this.avgAssistsPerGame = avgAssistsPerGame;
+        this.avgOffensiveReboundsPerGame = avgOffensiveReboundsPerGame;
+        this.avgDefensiveReboundsPerGame = avgDefensiveReboundsPerGame;
+        this.avgStealsPerGame = avgStealsPerGame;
+        this.avgBlocksPerGame = avgBlocksPerGame;
+    }
+
+    @Override
+    public String toString() {
+        return "BasketballPlayer{" +
+                "avgPointsPerGame=" + avgPointsPerGame +
+                ", avgAssistsPerGame=" + avgAssistsPerGame +
+                ", avgOffensiveReboundsPerGame=" + avgOffensiveReboundsPerGame +
+                ", avgDefensiveReboundsPerGame=" + avgDefensiveReboundsPerGame +
+                ", avgStealsPerGame=" + avgStealsPerGame +
+                ", avgBlocksPerGame=" + avgBlocksPerGame +
+                '}';
+    }
+
+    public BasketballPlayer(int rank, String sport, String firstName,
+                            String lastName, String hometown, String phoneNumber,
+                            String playerPosition, double height, int weight,
+                            String highSchool, boolean signed, String college) {
+        super(rank, sport, firstName, lastName, hometown, phoneNumber,
+                playerPosition, height, weight, highSchool, signed, college);
+    }
+
+    @Override
+    public void playerProfileCard() {
+        super.playerProfileCard();
+    }
+
+    @Override
+    public void fortyYardDash(double time) {
+        super.fortyYardDash(time);
+    }
+
+    @Override
+    public void squatMax(int weight) {
+        super.squatMax(weight);
+    }
+
+    @Override
+    public void benchMax(int weight) {
+        super.benchMax(weight);
+    }
 }
--- src/athleticScholarshipPackage/Main.java
+++ src/athleticScholarshipPackage/Main.java
@@ -2,14 +2,14 @@ package athleticScholarshipPackage;
 
 public class Main {
     public static void main(String[] args) {
-        Athlete test = new Athlete(1, "Soccer", "Mark",
+        BasketballPlayer mark = new BasketballPlayer(1, "Basketball", "Mark",
                 "Jacobs", "Columbia, SC", "803-333-5655",
-                "Forward", 5.8, 155,
+                "Power Forward", 6.6, 185,
                 "Spring Valley High School",
                 false, "University of South Carolina");
-        test.playerProfileCard();
-        test.fortyYardDash(4.7);
-        test.squatMax(501);
-        test.benchMax(500);
+        mark.playerProfileCard();
+        mark.fortyYardDash(4.7);
+        mark.squatMax(501);
+        mark.benchMax(500);
     }
 }
